2facdbb55c6db91a3b502f6d27712791500c356e
Emma Lindmark <emma_lindmark@live.se>
2022-02-17 16:01:19 +0100

    added imagesLoaded js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,12 @@
 var $grid = $('#item-list').isotope({
     // options
 });
+
+// layout Isotope after each image loads
+$grid.imagesLoaded().progress(function () {
+    $grid.isotope('layout');
+});
+
 // filter items on button click
 $('.filter-button-group').on('click', 'button', function () {
     var filterValue = $(this).attr('data-filter');
